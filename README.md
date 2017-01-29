0561e1bff9ac5a097bc6e733af6fedbb70eee0c6
toni_heittola <toni.heittola@gmail.com>
2017-01-29 18:37:52 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,168 @@
+Pelican-bdates - Important date listings for Pelican
+====================================================
+
+`pelican-bdates` is an open source Pelican plugin to produce important date listings from yaml data structures. The plugin is developed to be used with Markdown content and Bootstrap 3 based template. 
+
+**Author**
+
+Toni Heittola (toni.heittola@gmail.com), [GitHub](https://github.com/toni-heittola), [Home page](http://www.cs.tut.fi/~heittolt/)
+
+Installation instructions
+=========================
+
+## Requirements
+
+**bs4** is required. To ensure that all external modules are installed, run:
+
+    pip install -r requirements.txt
+
+**bs4** (BeautifulSoup) for parsing HTML content
+
+    pip install beautifulsoup4
+
+In order to regenerate minified CSS and JS files you need also: 
+
+**rcssmin** a CSS Minifier
+
+    pip install rcssmin
+    
+**jsmin** a JS Minifier
+
+    pip install jsmin
+
+## Pelican installation
+
+Make sure you include [Bootstrap](http://getbootstrap.com/) in your template.
+
+Make sure the directory where the plugin was installed is set in `pelicanconf.py`. For example if you installed in `plugins/pelican-bdates`, add:
+
+    PLUGIN_PATHS = ['plugins']
+
+Enable `pelican-bdates` with:
+
+    PLUGINS = ['pelican-bdates']
+
+To allow plugin in include css file, one needs to add following to the `base.html` template, in the head:
+
+    {% if article %}
+        {% if article.styles %}
+            {% for style in article.styles %}
+    {{ style }}
+            {% endfor %}
+        {% endif %}
+    {% endif %}
+    {% if page %}
+        {% if page.styles %}
+            {% for style in page.styles %}
+    {{ style }}
+            {% endfor %}
+        {% endif %}
+    {% endif %}
+
+Insert date listing in the page template:
+ 
+    {% if page.bdates %}
+        {{ page.bdates }}
+    {% endif %}
+
+Insert date listing in the article template:
+
+    {% if article.bdates %}
+        {{ article.bdates }}
+    {% endif %}
+
+Usage
+=====
+
+Date listing generation is triggered for the page either by setting BDATES metadata for the content (page or article) or using `<div>` with class `bdates`. 
+
+There is two layout modes available for both of these: `panel` and `list`. 
+
+## Date registry
+
+Example yaml-file:
+
+    - date: 9-11-2016
+      title: Test 1
+      category: category1
+    - date: 1-12-2016
+      title: Test 2
+      url: test2
+      category: category2
+
+## Parameters
+
+The parameters can be set in global, and content level. Globally set parameters are are first overwritten content meta data, and finally with div parameters.
+
+### Global parameters
+
+Parameters for the plugin can be set in `pelicanconf.py' with following parameters:
+
+| Parameter                 | Type      | Default       | Description  |
+|---------------------------|-----------|---------------|--------------|
+| BDATES_SOURCE             | String    |               | YAML-file to contain dates registry, see example format above. |
+| BDATES_TEMPLATE           | Dict of Jinja2 templates |  | Two templates can be set for panel and list  |
+| BDATES_ITEM_TEMPLATE      | Dict of Jinja2 templates |  | Two templates can be set for panel and list  |
+| BDATES_PANEL_COLOR        | String    | panel-primary |  CSS class used to color the panel template in the default template. Possible values: panel-default, panel-primary, panel-success, panel-info, panel-warning, panel-danger |
+| BDATES_CATEGORY_LABEL_CSS | Dict      |               | Dict with category labels as keys, second level dict with key`label-css`. |
+| BDATES_HEADER             | String    | Content       | Header text  |
+| BDATES_MINIFIED           | Boolean   | True          | Do we use minified CSS file. Disable in case of debugging.  |
+| BDATES_GENERATE_MINIFIED  | Boolean   | False         | CSS file is minified each time, Enable in case of development.   |
+
+### Content wise parameters
+
+| Parameter                 | Example value | Description  |
+|---------------------------|---------------|--------------|
+| BDATES                    | True          | Enable dates listing for the page | 
+| BDATES_SOURCE             | content/data/dates.yaml | Dates registry file |
+| BDATES_MODE               | panel         | Layout type, panel or list |
+| BDATES_PANEL_COLOR        | panel-info    | CSS class used to color the panel template in the default template. Possible values: panel-default, panel-primary, panel-success, panel-info, panel-warning, panel-danger |
+| BDATES_HEADER             | Personnel     | Header text  |
+| BDATES_CATEGORY           | cat1, cat2    | Comma separated list of categories shown |
+| BDATES_COUNT              | 3             | Count of most recent dates shown |
+| BDATES_SHOW_CATEGORIES    | True          | Show category label |
+| BDATES_DATE_FORMAT        | %d %B %Y      | Date format, see format supported by strftime |
+
+Example:
+
+    Title: Test page
+    Date: 2017-01-05 10:20
+    Category: test
+    Slug: test-page
+    Author: Test Person
+    bdates: True
+    bdates_header: Dates
+    bdates_category: category1
+    
+Date listing is available in template in variable `page.bdates` or `article.bdates`
+   
+### Div wise parameters
+
+| Parameter                 | Example value     | Description  |
+|---------------------------|-------------|--------------|
+| data-source               | content/data/dates.yaml | Date registry file
+| data-mode                 | panel       | Layout type, panel or list |
+| data-header               | Dates       | Header text |
+| data-category             | cat1, cat2 | Comma separated list of categories shown |
+| data-count                | 2 | Count of most recent dates shown |
+| data-show-categories      | True | Show category label |
+| data-date-format          | %d %B %Y | Date format, see format supported by strftime |
+| data-panel-color          | panel-info | CSS class used to color the panel template in the default template. Possible values: panel-default, panel-primary, panel-success, panel-info, panel-warning, panel-danger |
+Example listing:
+
+    <div class="bdates" data-source="content/data/dates.yaml" data-category="category1" data-mode="panel" data-header="Important dates" data-date-format="%d %B %Y"></div>
+        
+Example with meta fields:   
+
+    Title: Test page
+    Date: 2017-01-05 10:20
+    Category: test
+    Slug: test-page
+    Author: Test Person
+    bdates: True
+    bdates_category: category1
+    bdates_header: Important dates
+    bdates_panel_color: panel-default
+    bdates_source: content/data/dates.yaml
+    bdates_mode: panel
+    <div class="bdates"></div>
---
+++ bdates.py
@@ -0,0 +1,414 @@
+# -*- coding: utf-8 -*-
+"""
+Important date listings -- BDATES
+=================================
+Author: Toni Heittola (toni.heittola@gmail.com)
+
+"""
+
+import os
+import shutil
+import logging
+import copy
+from bs4 import BeautifulSoup
+from jinja2 import Template
+from pelican import signals, contents
+import datetime
+import yaml
+import operator
+
+logger = logging.getLogger(__name__)
+__version__ = '0.1.0'
+
+bdates_default_settings = {
+    'panel-color': 'panel-default',
+    'header': 'Dates',
+    'mode': 'panel',
+    'template': {
+        'panel': """
+            <div class="panel {{ panel_color }} hidden-print">
+              {% if header %}
+              <div class="panel-heading">
+                <h3 class="panel-title">{{header}}</h3>
+              </div>
+              {% endif %}
+              <ul class="bdates-container list-group">
+              {{list}}
+              </ul>
+            </div>
+         """,
+        'list': """
+            {% if header %}<h3 class="section-heading text-center">{{header}}</h3>{% endif %}
+            <div class="list-group bdates-container">
+            {{list}}
+            </div>
+        """},
+    'item-template': {
+        'panel': """
+            <a class="list-group-item {{item_color}}" href="{{site_url}}/{{item_url}}">
+            <div class="row">
+                <div class="col-md-12"><h5 class="list-group-item-heading {{item_css}}"><strong>{{item_date}}</strong></h5></div>
+                <div class="col-md-12"><h5 class="list-group-item-heading {{item_css}}">{{item_title}}</h5></div>
+            </div>
+            </a>
+            """,
+        'list': """<a class="list-group-item {{item_color}}" href="{{site_url}}/{{item_url}}">
+            <div class="row">
+                <div class="col-md-9"><h4 class="list-group-item-heading {{item_css}}">{{item_title}}</h4></div>
+                <div class="col-md-3"><h5 class="list-group-item-heading {{item_css}}"><strong>{{item_date}}</strong></h5></div>
+            </div>
+            </a>
+        """},
+    'data-source': None,
+    'category': None,
+    'count': None,
+    'past-dates': 'muted', # None, muted
+    'show': False,
+    'minified': True,
+    'generate_minified': True,
+    'show-categories': False,
+    'shorten-category-label': True,
+    'category-label-css': {},
+    'template-variable': False,
+    'site-url': '',
+    'date-format': '%d %b %Y',
+}
+
+bdates_settings = copy.deepcopy(bdates_default_settings)
+
+
+def load_dates_registry(source=None):
+    """
+
+    :param source: filename of the data file
+    :return: dates registry
+    """
+
+    if source and os.path.isfile(source):
+        try:
+            with open(source, 'r') as field:
+                dates = yaml.load(field)
+
+            if 'data' in dates:
+                dates = dates['data']
+
+            for item_id, item in enumerate(dates):
+                item['datetime'] = datetime.datetime.strptime(item['date'], '%d-%m-%Y')
+
+            dates.sort(key=operator.itemgetter('datetime'))
+            return dates
+
+        except ValueError:
+            logger.warn('`pelican-bdates` failed to load file [' + str(source) + ']')
+            return False
+
+    else:
+        logger.warn('`pelican-bdates` failed to load file [' + str(source) + ']')
+        return False
+
+
+def get_attribute(attrs, name, default=None):
+    """
+    Get div attribute
+    :param attrs: attribute dict
+    :param name: name field
+    :param default: default value
+    :return: value
+    """
+
+    if 'data-'+name in attrs:
+        return attrs['data-'+name]
+    else:
+        return default
+
+
+def item_link(item, settings):
+    past_date = item['datetime'] > datetime.datetime.now()
+    if not past_date:
+        item_css = 'text-muted'
+        item_color = ''
+    else:
+        item_css = ''
+        # only use colors for upcoming dates
+        if 'color' in item:
+            item_color = item['color']
+        else:
+            item_color = ''
+
+    template = Template(settings['item-template'][settings['mode']].strip('\t\r\n').replace('&gt;', '>').replace('&lt;', '<'))
+    html = BeautifulSoup(template.render(site_url=settings['site-url'],
+                                         item_css=item_css,
+                                         item_url=item['url'] if 'url' in item else '',
+                                         item_title=item['title'],
+                                         item_date=item['datetime'].strftime(settings['date-format']),
+                                         item_color=item_color,
+                                         ), "html.parser")
+
+    return html.decode()
+
+
+def generate(settings):
+    dates = load_dates_registry(source=settings['data-source'])
+    if dates:
+        html = "\n"
+        count = 0
+        for item_id, item in enumerate(dates):
+            if settings['count'] and count < settings['count']:
+                if 'category' in bdates_settings and settings['category']:
+                    if item['category'] in settings['category']:
+                        html += item_link(item=item, settings=settings) + "\n"
+                        count += 1
+                else:
+                    html += item_link(item=item, settings=settings) + "\n"
+                    count += 1
+            else:
+                if 'category' in settings and settings['category']:
+                    if item['category'] in settings['category']:
+                        html += item_link(item=item, settings=settings) + "\n"
+                        count += 1
+                else:
+                    html += item_link(item=item, settings=settings) + "\n"
+                    count += 1
+        html += "\n"
+        template = Template(settings['template'][settings['mode']].strip('\t\r\n').replace('&gt;', '>').replace('&lt;', '<'))
+
+        if count:
+            return BeautifulSoup(template.render(list=html,
+                                                 header=settings['header'],
+                                                 site_url=settings['site-url'],
+                                                 panel_color=settings['panel-color'],), "html.parser")
+        else:
+            return ''
+
+
+def bdates(content):
+    """
+    Main processing
+
+    """
+
+    if isinstance(content, contents.Static):
+        return
+
+    soup = BeautifulSoup(content._content, 'html.parser')
+
+    # Template variable
+    if bdates_settings['template-variable']:
+        # We have page variable set
+        bdates_settings['show'] = True
+        div_html = generate(settings=bdates_settings)
+        content.bdates = div_html.decode()
+
+    else:
+        content.bdates = None
+
+    bdates_divs = soup.find_all('div', class_='bdates')
+
+    if bdates_divs:
+        bdates_settings['show'] = True
+
+        for bdates_div in bdates_divs:
+            # We have div in the page
+            settings = copy.deepcopy(bdates_settings)
+            settings['data-source'] = get_attribute(bdates_div.attrs, 'source', bdates_settings['data-source'])
+            settings['mode'] = get_attribute(bdates_div.attrs, 'mode', bdates_settings['mode'])
+            settings['header'] = get_attribute(bdates_div.attrs, 'header', bdates_settings['header'])
+            settings['category'] = get_attribute(bdates_div.attrs, 'category', bdates_settings['category'])
+            if settings['category'] and isinstance(settings['category'], basestring):
+                settings['category'] = [x.strip() for x in settings['category'].split(',')]
+
+            settings['count'] = get_attribute(bdates_div.attrs, 'count', bdates_settings['count'])
+            if settings['count']:
+                settings['count'] = int(settings['count'])
+
+            settings['panel-color'] = get_attribute(bdates_div.attrs, 'panel-color', bdates_default_settings['panel-color'])
+            settings['show-categories'] = get_attribute(bdates_div.attrs, 'show-categories', bdates_default_settings['show-categories'])
+            settings['date-format'] = get_attribute(bdates_div.attrs, 'date-format', bdates_default_settings['date-format'])
+
+            div_html = generate(settings=settings)
+            bdates_div.replaceWith(div_html)
+
+    if bdates_settings['show']:
+
+        if bdates_settings['minified']:
+            html_elements = {
+                'css_include': ['<link rel="stylesheet" href="' + bdates_settings['site-url'] + '/theme/css/bdates.min.css">']
+             }
+        else:
+            html_elements = {
+                'css_include': ['<link rel="stylesheet" href="' + bdates_settings['site-url'] + '/theme/css/bdates.css">']
+            }
+
+        if u'scripts' not in content.metadata:
+            content.metadata[u'scripts'] = []
+
+        if u'styles' not in content.metadata:
+            content.metadata[u'styles'] = []
+        for element in html_elements['css_include']:
+            if element not in content.metadata[u'styles']:
+                content.metadata[u'styles'].append(element)
+
+    content._content = soup.decode()
+
+
+def process_page_metadata(generator, metadata):
+    """
+    Process page metadata and assign css and styles
+
+    """
+
+    global bdates_default_settings, bdates_settings
+    bdates_settings = copy.deepcopy(bdates_default_settings)
+
+    if u'styles' not in metadata:
+        metadata[u'styles'] = []
+    if u'scripts' not in metadata:
+        metadata[u'scripts'] = []
+
+    if u'bdates' in metadata and (metadata['bdates'] == 'True' or metadata['bdates'] == 'true'):
+        bdates_settings['show'] = True
+        bdates_settings['template-variable'] = True
+    else:
+        bdates_settings['show'] = False
+        bdates_settings['template-variable'] = False
+
+    if u'bdates_source' in metadata:
+        bdates_settings['data-source'] = metadata['bdates_source']
+
+    if u'bdates_mode' in metadata:
+        bdates_settings['mode'] = metadata['bdates_mode']
+
+    if u'bdates_panel_color' in metadata:
+        bdates_settings['panel_color'] = metadata['bdates_panel_color']
+
+    if u'bdates_header' in metadata:
+        bdates_settings['header'] = metadata['bdates_header']
+
+    if u'bdates_category' in metadata:
+        bdates_settings['category'] = [x.strip() for x in metadata['bdates_category'].split(',')]
+
+    if u'bdates_count' in metadata:
+        bdates_settings['count'] = metadata['bdates_count']
+        if bdates_settings['count']:
+            bdates_settings['count'] = int(bdates_settings['count'])
+
+    if u'bdates_show_categories' in metadata:
+        bdates_settings['show-categories'] = metadata['bdates_show_categories']
+
+    if u'bdates_date_format' in metadata:
+        bdates_settings['date-format'] = metadata['bdates_date_format']
+
+
+def move_resources(gen):
+    """
+    Move files from js/css folders to output folder, use minified files.
+
+    """
+
+    plugin_paths = gen.settings['PLUGIN_PATHS']
+    if bdates_settings['minified']:
+        if bdates_settings['generate_minified']:
+            minify_css_directory(gen=gen, source='css', target='css.min')
+
+        css_target = os.path.join(gen.output_path, 'theme', 'css', 'bdates.min.css')
+        if not os.path.exists(os.path.join(gen.output_path, 'theme', 'css')):
+            os.makedirs(os.path.join(gen.output_path, 'theme', 'css'))
+
+        for path in plugin_paths:
+            css_source = os.path.join(path, 'pelican-bdates', 'css.min', 'bdates.min.css')
+
+            if os.path.isfile(css_source):
+                shutil.copyfile(css_source, css_target)
+
+            if os.path.isfile(css_target):
+                break
+    else:
+        css_target = os.path.join(gen.output_path, 'theme', 'css', 'bdates.css')
+        if not os.path.exists(os.path.join(gen.output_path, 'theme', 'css')):
+            os.makedirs(os.path.join(gen.output_path, 'theme', 'css'))
+
+        for path in plugin_paths:
+            print path
+            css_source = os.path.join(path, 'pelican-bdates', 'css', 'bdates.css')
+
+            if os.path.isfile(css_source):
+                shutil.copyfile(css_source, css_target)
+
+            if os.path.isfile(css_target):
+                break
+
+
+def minify_css_directory(gen, source, target):
+    """
+    Move CSS resources from source directory to target directory and minify. Using rcssmin.
+
+    """
+    import rcssmin
+
+    plugin_paths = gen.settings['PLUGIN_PATHS']
+    for path in plugin_paths:
+        source_ = os.path.join(path, 'pelican-bdates', source)
+        target_ = os.path.join(path, 'pelican-bdates', target)
+        if os.path.isdir(source_):
+            if not os.path.exists(target_):
+                os.makedirs(target_)
+
+            for root, dirs, files in os.walk(source_):
+                for current_file in files:
+                    if current_file.endswith(".css"):
+                        current_file_path = os.path.join(root, current_file)
+                        with open(current_file_path) as css_file:
+                            with open(os.path.join(target_, current_file.replace('.css', '.min.css')), "w") as minified_file:
+                                minified_file.write(rcssmin.cssmin(css_file.read(), keep_bang_comments=True))
+
+
+def init_default_config(pelican):
+    """
+    Handle settings from pelicanconf.py
+
+    """
+
+    global bdates_default_settings, bdates_settings
+
+    bdates_default_settings['site-url'] = pelican.settings['SITEURL']
+
+    if 'BDATES_SOURCE' in pelican.settings:
+        bdates_default_settings['data-source'] = pelican.settings['BDATES_SOURCE']
+
+    if 'BDATES_HEADER' in pelican.settings:
+        bdates_default_settings['header'] = pelican.settings['BDATES_HEADER']
+
+    if 'BDATES_TEMPLATE' in pelican.settings:
+        bdates_default_settings['template'].update(pelican.settings['BDATES_TEMPLATE'])
+
+    if 'BDATES_ITEM_TEMPLATE' in pelican.settings:
+        bdates_default_settings['item-template'].update(pelican.settings['BDATES_ITEM_TEMPLATE'])
+
+    if 'BDATES_CATEGORY_LABEL_CSS' in pelican.settings:
+        bdates_default_settings['category-label-css'] = pelican.settings['BDATES_CATEGORY_LABEL_CSS']
+
+    if 'BDATES_PANEL_COLOR' in pelican.settings:
+        bdates_default_settings['panel-color'] = pelican.settings['BDATES_PANEL_COLOR']
+
+    if 'BDATES_MINIFIED' in pelican.settings:
+        bdates_default_settings['minified'] = pelican.settings['BDATES_MINIFIED']
+
+    if 'BDATES_GENERATE_MINIFIED' in pelican.settings:
+        bdates_default_settings['generate_minified'] = pelican.settings['BDATES_GENERATE_MINIFIED']
+
+    bdates_settings = copy.deepcopy(bdates_default_settings)
+
+
+def register():
+    """
+    Register signals
+
+    """
+
+    signals.initialized.connect(init_default_config)
+    signals.article_generator_context.connect(process_page_metadata)
+    signals.page_generator_context.connect(process_page_metadata)
+    signals.article_generator_finalized.connect(move_resources)
+
+    signals.content_object_init.connect(bdates)
+
