24fa4bd83a778e8afaf8646ade40dead4f8899f6
toni-heittola <toni.heittola@gmail.com>
2025-06-10 12:50:55 +0300

    Fix missing key error with content.metadata

--- bdates.py
+++ bdates.py
@@ -474,12 +474,15 @@ def bdates(content):
                 'css_include': ['<link rel="stylesheet" href="' + bdates_settings['site-url'] + '/theme/css/bdates.css">']
             }
 
-        if u'scripts' not in content.metadata:
-            content.metadata[u'scripts'] = []
+        for element in html_elements['js_include']:
+            if 'scripts' not in content.metadata:
+                content.metadata[u'scripts'] = []
+            if element not in content.metadata[u'scripts']:
+                content.metadata[u'scripts'].append(element)
 
-        if u'styles' not in content.metadata:
-            content.metadata[u'styles'] = []
         for element in html_elements['css_include']:
+            if 'styles' not in content.metadata:
+                content.metadata[u'styles'] = []
             if element not in content.metadata[u'styles']:
                 content.metadata[u'styles'].append(element)
 
